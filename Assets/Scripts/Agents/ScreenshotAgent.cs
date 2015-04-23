f26a5349466447629197451b77579a40b1c2e6e1
Nolan Filter <nolan.f.filter@gmail.com>
2015-04-23 12:45:01 -0400

    WIP implemented Best Practices features

---
+++ Assets/Scripts/Agents/ScreenshotAgent.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenshotAgent : MonoBehaviour {
+
+	public delegate void PostRenderFinish();
+	public event PostRenderFinish OnPostRenderFinish;
+
+	private Texture2D texture;
+
+	private static ScreenshotAgent mInstance = null;
+	public static ScreenshotAgent instance
+	{
+		get
+		{
+			return mInstance;
+		}
+	}
+	
+	void Awake()
+	{
+		if( mInstance != null )
+		{
+			Debug.LogError( string.Format( "Only one instance of ScreenshotAgent allowed! Destroying:" + gameObject.name +", Other:" + mInstance.gameObject.name ) );
+			return;
+		}
+		
+		mInstance = this;
+
+		texture = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+		Disable();
+	}
+
+	void OnPostRender()
+	{
+		texture.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+		texture.Apply();
+		Disable();
+		
+		if( OnPostRenderFinish != null )
+			OnPostRenderFinish();
+	}	
+
+	public static void Enable()
+	{
+		if( instance )
+			instance.enabled = true;
+	}
+
+	public static void Disable()
+	{
+		if( instance )
+			instance.enabled = false;
+	}
+
+	public static Texture2D GetTexture()
+	{
+		if( instance )
+			return instance.texture;
+
+		return null;
+	}	
+}
--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.Advertisements;
+using UnityEngine.iOS;
 using System.Collections;
+using UnionAssets.FLE;
 
 public class GameController : MonoBehaviour {
 
@@ -13,14 +15,41 @@ public class GameController : MonoBehaviour {
 	private int[] deck;
 
 	private float colorOffset;
-
-	private const string gameID = "32974";
+	
+	private const string iosGameID = "32974";
+	private const string androidGameID = "33171";
+	private string gameID;
 
 	private int numTimesPrinted = 0;
 
+	private int mode = 0;
+
 	void Start()
 	{
+#if UNITY_IOS
+		gameID = iosGameID;
+#elif UNITY_ANDROID
+		gameID = androidGameID;
+#endif
+
 		Advertisement.Initialize( gameID, true );
+		Advertisement.allowPrecache = true;
+
+		iAdBanner banner = iAdBannerController.instance.CreateAdBanner( TextAnchor.LowerLeft );
+
+		UnityEngine.iOS.NotificationServices.RegisterForNotifications( NotificationType.Alert | NotificationType.Badge | NotificationType.Sound );
+
+		//IOSNotificationController.instance.RequestNotificationPermissions();
+
+		/*
+		IOSNotificationController.instance.OnNotificationScheduleResult += OnNotificationScheduleResult;
+
+		IOSNotificationController.instance.ScheduleNotification (5, "Your Notification Text No Sound", false);
+		*/
+
+		IOSNotificationController.instance.RegisterForRemoteNotifications( NotificationType.Alert | NotificationType.Badge | NotificationType.Sound );
+
+		IOSNotificationController.instance.addEventListener( IOSNotificationController.DEVICE_TOKEN_RECEIVED, OnTokenReceived );
 
 		deck = new int[ BoardAgent.BoardSize ];
 	}
@@ -35,12 +64,64 @@ public class GameController : MonoBehaviour {
 		FingerGestures.OnFingerDown -= OnPress;
 	}
 
+	/*
+	bool tokenSent = false;
+	void  FixedUpdate () {
+		
+		if (!tokenSent) { // tokenSent needs to be defined somewhere (bool tokenSent = false)
+
+			byte[] token   =  UnityEngine.iOS.NotificationServices.deviceToken;
+			
+			if(token != null) {
+				
+				string tokenString =  System.BitConverter.ToString(token).Replace("-", "").ToLower();
+				
+				Debug.Log ("OnTokenReceived");
+				
+				Debug.Log (tokenString);
+				
+			}
+			
+		}
+	}
+	*/
+
+	private void OnNotificationScheduleResult( ISN_Result res ) {
+		IOSNotificationController.instance.OnNotificationScheduleResult -= OnNotificationScheduleResult;
+		string msg = string.Empty;
+		if(res.IsSucceeded) {
+			msg += "Notification was successfully scheduled \n allowed notifications types: \n";
+			if((IOSNotificationController.AllowedNotificationsType & IOSUIUserNotificationType.Alert) != 0) {
+				msg += "Alert ";
+			}
+			if((IOSNotificationController.AllowedNotificationsType & IOSUIUserNotificationType.Sound) != 0) {
+				msg += "Sound ";
+			}
+			if((IOSNotificationController.AllowedNotificationsType & IOSUIUserNotificationType.Badge) != 0) {
+				msg += "Badge ";
+			}
+		} else {
+			msg += "Notification scheduling failed";
+		}
+		
+		IOSMessage.Create("On Notification Schedule Result", msg);
+	}
+
+	private void OnTokenReceived( CEvent e )
+	{
+		IOSNotificationDeviceToken token = e.data as IOSNotificationDeviceToken;
+		
+		Debug.Log( "OnTokenReceived" );
+		
+		Debug.Log( token.tokenString );
+
+		IOSNotificationController.instance.removeEventListener( IOSNotificationController.DEVICE_TOKEN_RECEIVED, OnTokenReceived );
+	}
+
 	private void OnPress( int fingerIndex, Vector2 fingerPos )
 	{
 		if( isRunning )
 		{
-			return;
-
 			StopCoroutine( "DoPrint" );
 
 			int deckIndex;
@@ -48,13 +129,17 @@ public class GameController : MonoBehaviour {
 			while( index < BoardAgent.BoardSize )
 			{
 				deckIndex = deck[index];
-				ActivateSprite( new Vector2( deckIndex % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - deckIndex / BoardAgent.BoardWidth ) );
-				//ActivateSprite( new Vector2( i % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - i / BoardAgent.BoardWidth ) );
-			
+
+				switch( mode )
+				{
+					case 0: ActivateSprite( new Vector2( index % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - index / BoardAgent.BoardWidth ) ); break;
+					case 1: ActivateSprite( new Vector2( deckIndex % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - deckIndex / BoardAgent.BoardWidth ) ); break;
+				}
+					
 				index++;
 			}
 
-			isRunning = false;
+			StartCoroutine( "FinishPrint" );
 		}
 		else
 		{
@@ -73,12 +158,12 @@ public class GameController : MonoBehaviour {
 
 	private IEnumerator DoPrint()
 	{
-		isRunning = true;
-
 		while( Advertisement.isShowing )
 		{
 			yield return null;
 		}
+
+		isRunning = true;
 
 		index = 0;
 		int deckIndex;
@@ -91,8 +176,12 @@ public class GameController : MonoBehaviour {
 			while( currentDistance >= 1f )
 			{
 				deckIndex = deck[index];
-				//ActivateSprite( new Vector2( deckIndex % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - deckIndex / BoardAgent.BoardWidth ) );
-				ActivateSprite( new Vector2( index % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - index / BoardAgent.BoardWidth ) );
+
+				switch( mode )
+				{
+					case 0: ActivateSprite( new Vector2( index % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - index / BoardAgent.BoardWidth ) ); break;
+					case 1: ActivateSprite( new Vector2( deckIndex % BoardAgent.BoardWidth, BoardAgent.BoardHeight - 1 - deckIndex / BoardAgent.BoardWidth ) ); break;
+				}
 
 				index++;
 				currentDistance -= 1f;
@@ -104,9 +193,20 @@ public class GameController : MonoBehaviour {
 			yield return null;
 		}
 
-		numTimesPrinted++;
+		StartCoroutine( "FinishPrint" );
+	}
 
+	private IEnumerator FinishPrint()
+	{
+		numTimesPrinted++;
+		
 		isRunning = false;
+		
+		ScreenshotAgent.Enable();
+
+		yield return null;
+
+		IOSSocialManager.instance.ShareMedia( "Check this rad print!", ScreenshotAgent.GetTexture() );
 	}
 
 	private void ActivateSprite( Vector2 position )
