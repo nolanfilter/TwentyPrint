2086e3c1a24496a11c3b6969fc2105b0aa62344a
Nolan Filter <nfilter@ogmento.com>
2015-06-11 15:40:00 -0400

    Fix for Unity 5.1 + Analytics crash

---
+++ Assets/Analytics51Fix.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Analytics51Fix  {
+	
+	private void DoNotCall () {
+		// Prevent analytics from being stripped.
+		typeof(UnityEngine.Analytics.Analytics).ToString();
+	}
+}
